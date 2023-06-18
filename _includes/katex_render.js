b82082dc838f1c11a9b9654c3384dca8b9addd7c
braydenlook <93011025+braydenlook@users.noreply.github.com>
2023-06-18 11:31:41 -0600

    Update katex_render.js

--- _includes/katex_render.js
+++ _includes/katex_render.js
@@ -1,15 +1,15 @@
-<script> $("script[type='math/tex']").replaceWith(
+$("script[type='math/tex']").replaceWith(
   function(){
     var tex = $(this).text();
     return "<span class=\"inline-equation\">" + 
            katex.renderToString(tex) +
            "</span>";
-}); </script>
+});
 
-<script>$("script[type='math/tex; mode=display']").replaceWith(
+$("script[type='math/tex; mode=display']").replaceWith(
   function(){
     var tex = $(this).text();
     return "<div class=\"equation\">" + 
            katex.renderToString("\\displaystyle "+tex) +
            "</div>";
-});</script>
+});
